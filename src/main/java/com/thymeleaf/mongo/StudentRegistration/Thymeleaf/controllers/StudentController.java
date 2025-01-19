5bb9abede19b04093e7f90359dcd54744d2b6972
Kaung Zaw Hein <kzheindev789@gmail.com>
2025-01-19 18:11:50 +0630

    Add Exception Handling to Contrller AND RestController

--- src/main/java/com/thymeleaf/mongo/StudentRegistration/Thymeleaf/controllers/StudentController.java
+++ src/main/java/com/thymeleaf/mongo/StudentRegistration/Thymeleaf/controllers/StudentController.java
@@ -3,15 +3,20 @@ package com.thymeleaf.mongo.StudentRegistration.Thymeleaf.controllers;
 
 import com.thymeleaf.mongo.StudentRegistration.Thymeleaf.models.Students;
 import com.thymeleaf.mongo.StudentRegistration.Thymeleaf.services.StudentService;
+import jakarta.servlet.http.HttpServletRequest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.sql.Timestamp;
+import java.time.LocalDateTime;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Controller
 @SuppressWarnings("unused")
@@ -34,11 +39,18 @@ public class StudentController {
         return "index";
     }
 
-    // ADD STUDENT
-    @PostMapping
-    public Students addStudent(@RequestBody Students student) {
-        logger.info(student.toString());
-        return  studentService.addStudent(student);
+    // ADD STUDENT ROUTE
+    @GetMapping("add")
+    public String addStudentRoute(Model m) {
+        m.addAttribute("student", new Students());
+        return  "addStudent";
+    }
+
+    // ADD STUDENT DATA
+    @PostMapping("add")
+    public String addStudentForm(@RequestBody Students student, Model m) {
+        m.addAttribute("message", "Add Student Successfully.");
+        return  "addStudent";
     }
 
     // GET ALL STUDENTS
@@ -59,9 +71,20 @@ public class StudentController {
         studentService.deleteStudent(id);
     }
 
+    @GetMapping("/error")
+    public String testError() {
+        throw new RuntimeException("Error Testing");
+    }
+
 
     @ExceptionHandler(Exception.class)
-    public ResponseEntity<String> handleException(Exception e) {
-        return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+    public ResponseEntity<Map<String, Object>> handleException(Exception e, HttpServletRequest request) {
+        Map<String, Object> errorDetails = new HashMap<>();
+        errorDetails.put("timestamp", LocalDateTime.now());
+        errorDetails.put("status", HttpStatus.INTERNAL_SERVER_ERROR);
+        errorDetails.put("error","INTERNAL SERVER ERROR");
+        errorDetails.put("message", e.getMessage());
+        errorDetails.put("path", request.getRequestURI());
+        return new ResponseEntity<>(errorDetails, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
--- src/main/java/com/thymeleaf/mongo/StudentRegistration/Thymeleaf/controllers/StudentRestController.java
+++ src/main/java/com/thymeleaf/mongo/StudentRegistration/Thymeleaf/controllers/StudentRestController.java
@@ -2,11 +2,17 @@ package com.thymeleaf.mongo.StudentRegistration.Thymeleaf.controllers;
 
 import com.thymeleaf.mongo.StudentRegistration.Thymeleaf.models.Students;
 import com.thymeleaf.mongo.StudentRegistration.Thymeleaf.services.StudentService;
+import jakarta.servlet.http.HttpServletRequest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDateTime;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @SuppressWarnings("unused")
@@ -46,5 +52,21 @@ public class StudentRestController {
     @DeleteMapping
     public void deleteStudent(@RequestParam("id") String id) {
         studentService.deleteStudent(id);
+    }
+
+    @GetMapping("/error")
+    public String testError() {
+        throw new RuntimeException("Error Testing");
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<Map<String, Object>> handleException(Exception e, HttpServletRequest request) {
+        Map<String, Object> errorDetails = new HashMap<>();
+        errorDetails.put("timestamp", LocalDateTime.now());
+        errorDetails.put("status", HttpStatus.INTERNAL_SERVER_ERROR);
+        errorDetails.put("error","INTERNAL SERVER ERROR");
+        errorDetails.put("message", e.getMessage());
+        errorDetails.put("path", request.getRequestURI());
+        return new ResponseEntity<>(errorDetails, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
