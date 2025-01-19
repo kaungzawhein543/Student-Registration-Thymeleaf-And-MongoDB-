79b0e5ba0b06a4270c09e5753414fcf63962e8eb
Kaung Zaw Hein <kzheindev789@gmail.com>
2025-01-19 15:32:06 +0630

    write students services about students

--- src/main/java/com/thymeleaf/mongo/StudentRegistration/Thymeleaf/models/Students.java
+++ src/main/java/com/thymeleaf/mongo/StudentRegistration/Thymeleaf/models/Students.java
@@ -3,11 +3,15 @@ package com.thymeleaf.mongo.StudentRegistration.Thymeleaf.models;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Document(collation = "students")
 public class Students {
+    @Id
     private int id;
     private String name;
     private String brith_of_date;
--- src/main/java/com/thymeleaf/mongo/StudentRegistration/Thymeleaf/services/StudentService.java
+++ src/main/java/com/thymeleaf/mongo/StudentRegistration/Thymeleaf/services/StudentService.java
@@ -1,12 +1,52 @@
 package com.thymeleaf.mongo.StudentRegistration.Thymeleaf.services;
 
+import com.thymeleaf.mongo.StudentRegistration.Thymeleaf.models.Students;
 import com.thymeleaf.mongo.StudentRegistration.Thymeleaf.repositories.StudentRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import java.util.List;
+import java.util.Optional;
+
+@SuppressWarnings("unused")
+@Slf4j
 public class StudentService {
 
-    private StudentRepository studentRepository;
+    // STUDENT REPOSITORY
+    private final StudentRepository studentRepository;
+
+    // GET LOGGER
+    private static final Logger logger = LoggerFactory.getLogger(StudentService.class);
 
     public StudentService(StudentRepository studentRepository) {
         this.studentRepository = studentRepository;
+    }
+
+    // ADD STUDENTS
+    public Students addStudent(Students student) {
+        return  studentRepository.save(student);
+    }
+
+    // GET ALL STUDENTS
+    public List<Students> getAllStudents() {
+        return studentRepository.findAll();
+    }
+
+    // UPDATE STUDENT WITH ID
+    public void updateStudent(Students student) {
+        Optional<Students> studentFromDb =  studentRepository.findById(student.getId());
+
+        if(studentFromDb.isPresent()) {
+             studentRepository.save(student);
+        }else {
+            logger.error("STUDENT NOT FOUND ERROR IN SERVICE CLASS IN UPDATE METHOD");
+            throw new RuntimeException("Student Not Found");
+        }
+    }
+
+    // DELETE STUDENT WITH ID
+    public void deleteStudent(int id) {
+        studentRepository.deleteById(id);
     }
 }
