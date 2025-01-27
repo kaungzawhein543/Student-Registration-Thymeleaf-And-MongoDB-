9d9e701cf651585b93cd77bab1a7d42f7b5d596a
Kaung Zaw Hein <kzheindev789@gmail.com>
2025-01-27 20:25:19 +0630

    fixed message error add class

--- src/main/java/com/thymeleaf/mongo/StudentRegistration/Thymeleaf/controllers/ClassController.java
+++ src/main/java/com/thymeleaf/mongo/StudentRegistration/Thymeleaf/controllers/ClassController.java
@@ -6,6 +6,7 @@ import com.thymeleaf.mongo.StudentRegistration.Thymeleaf.services.StudentService
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 
@@ -23,12 +24,12 @@ public class ClassController {
 
     // ADD CLASS
     @PostMapping("add")
-    public String save(@ModelAttribute Class classObj,Model m) {
+    public String save(@ModelAttribute Class classObj, RedirectAttributes redirectAttributes) {
           Class resultClass = classService.save(classObj);
           if(resultClass != null) {
-              m.addAttribute("message","Add Class Successfully");
-          }else {
-              m.addAttribute("errorMessage","Add Class Failed");
+              redirectAttributes.addFlashAttribute("message","Add Class Successfully");
+          } else {
+              redirectAttributes.addFlashAttribute("errorMessage","Add Class Failed");
           }
         return "redirect:/classes/addClass";
     }
@@ -37,6 +38,11 @@ public class ClassController {
     @GetMapping("addClass")
     public String save(Model m) {
         m.addAttribute("class", new Class());
+        if(m.containsAttribute("message")) {
+            m.addAttribute("message", m.getAttribute("message"));
+        }else if(m.containsAttribute("errorMessage")) {
+            m.addAttribute("errorMessage", m.getAttribute("errorMessage"));
+        }
         return "addClass";
     }
 
